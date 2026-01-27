be734f74f0ed21a2542a16f1b460c1d4a3e46169
rt4817 <joel.ag789@gmail.com>
2026-01-27 22:06:06 +1100

    Custom css, adblock settings

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 config.load_autoconfig()
+
+c.content.blocking.method = 'both'
+c.content.blocking.enabled = True
+
 config.bind('<Ctrl+m>', 'spawn mpv {url}', mode='normal')
 config.bind('<Ctrl+Shift+m>', 'hint links spawn mpv {hint-url}', mode='normal')
 
+config.set('content.user_stylesheets', ['~/.config/qutebrowser/css'])
