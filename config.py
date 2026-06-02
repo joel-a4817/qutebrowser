2d77d0b92f9ce877b96420524b68aeb3723536f1
rt4817 <joel.ag789@gmail.com>
2026-06-02 11:47:00 +1000

    google login fix

--- config.py
+++ config.py
@@ -8,5 +8,15 @@ c.qt.args = [
     "disable-gpu-rasterization",
 ]
 
+# Enable qutebrowser's built-in fixes
+config.set("content.site_specific_quirks.enabled", True)
+
+# Force Firefox UA ONLY for Google login (this is what worked for you)
+config.set(
+    "content.headers.user_agent",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:115.0) Gecko/20100101 Firefox/115.0",
+    "https://accounts.google.com/*"
+)
+
 config.bind('<Ctrl+m>', 'spawn mpv {url}', mode='normal')
 config.bind('<Ctrl+Shift+m>', 'hint links spawn mpv {hint-url}', mode='normal')
